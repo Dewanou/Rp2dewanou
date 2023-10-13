f7d1a610ad4fdb15e68994723bf49d386748a879
Dewanou <105568509+Dewanou@users.noreply.github.com>
2023-10-13 19:34:36 +0200

    Update fichier_dashboard.py

--- fichier_dashboard.py
+++ fichier_dashboard.py
@@ -5,6 +5,7 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import joblib
 import numpy as np
+from functions import *
 
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.SKETCHY])
 
@@ -12,11 +13,11 @@ server= app.server
 
 
 # Charger le modèle préalablement exporté
-best_model = joblib.load('model.pkl')
+#best_model = joblib.load('model.pkl')
 
 
 
-data_for_predictions = pd.read_csv('data_for_predictions.csv').set_index('SK_ID_CURR')
+#data_for_predictions = pd.read_csv('data_for_predictions.csv').set_index('SK_ID_CURR')
 
 
 # Charger les données de data_affich et data_disply
@@ -26,15 +27,15 @@ data_affich = pd.read_csv('df_affich.csv').set_index('SK_ID_CURR')
 data_disply = pd.read_csv('data_disply.csv').set_index('SK_ID_CURR')
 
 # Fonction pour effectuer la prédiction
-def predict(sk_id_curr):
+#def predict(sk_id_curr):
     # Récupérer les données correspondant à SK_ID_CURR depuis la base de données
-    data = data_for_predictions.loc[int(sk_id_curr)]
+   # data = data_for_predictions.loc[int(sk_id_curr)]
 
     # Effectuer la prédiction
-    prediction = best_model.predict(data.values.reshape(1, -1))
-    probabilities = best_model.predict_proba(data.values.reshape(1, -1))  # Pour obtenir les probabilités des classes
+   # prediction = best_model.predict(data.values.reshape(1, -1))
+   # probabilities = best_model.predict_proba(data.values.reshape(1, -1))  # Pour obtenir les probabilités des classes
 
-    return prediction[0], probabilities[0]
+    # return prediction[0], probabilities[0]
 
 app.layout = html.Div([
         dbc.Navbar(
