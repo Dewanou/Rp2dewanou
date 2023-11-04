99d4d04ed4615d12d93bb30c9e276857f8336091
Dewanou <105568509+Dewanou@users.noreply.github.com>
2023-11-04 22:28:11 +0100

    Create test_page_1.py

---
+++ tests/test_page_1.py
@@ -0,0 +1,22 @@
+# test_page_1.py
+
+import dash.testing.wait as wait
+import dash.testing.browser
+
+def test_page_1():
+    # Créez une instance de navigateur Dash
+    browser = dash.testing.browser.Dash(__name__)
+    browser.server.app.config.suppress_callback_exceptions = True
+
+    # Chargez la page de la page 1 (assurez-vous que l'URL est correcte)
+    browser.visit('https://dewanoupredict-9816cc161f92.herokuapp.com/')
+
+    # Attendez que la page soit complètement chargée (vous pouvez ajuster le délai si nécessaire)
+    wait.until(lambda: len(browser.find_elements("#sk-id-dropdown")) > 0)
+
+    # Effectuez des assertions pour vérifier le contenu de la page
+    assert "Prédiction de la Solvabilité du Client" in browser.title
+    assert len(browser.find_elements("#sk-id-dropdown")) > 0
+
+    # Fermez le navigateur
+    browser.quit()
